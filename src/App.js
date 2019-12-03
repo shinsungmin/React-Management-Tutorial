ca23c8ba66fabdbd8fd2a6807f41152c1fdece24
shin sung min <shinandmin@connected8.com>
2019-12-03 13:47:48 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './component/Customer'
+
+const customer = {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name': '홍길동',
+    'birthday': '861231',
+    'gender': '남자',
+    'job': '대학생'
+}
 
 class App extends Component {
     render() {
         return(
-            <div className="gray-background">
-                <img src={logo} lat="logo" />
-                <h2>Let's develop management system!</h2>
-            </div>
+            <Customer
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birthday}
+                gender={customer.gender}
+                job={customer.job}
+            />
         )
     }
 }
